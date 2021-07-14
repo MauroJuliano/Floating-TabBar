c5473ad7f216707b343306438afb360248e4d4f9
MauroJuliano <43898163+MauroJuliano@users.noreply.github.com>
2021-07-14 15:55:47 -0400

    Initial Commit

---
+++ Floating-TabBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Floating-TabBar
+//
+//  Created by user195713 on 7/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
